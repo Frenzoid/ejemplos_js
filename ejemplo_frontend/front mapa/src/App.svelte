ebec4478f12e56b1744f2aa9b32801a6ce0c444f
Frenzod <frenzoid@protonmail.com>
2022-12-07 01:58:43 +0100

    añadido un front mostrando un mapa interactivo

---
+++ ejemplo_frontend/front mapa/src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+  // Matrix 20 x 20
+  let matrix = [];
+
+  for (let i = 0; i < 20; i++) {
+    matrix[i] = [];
+    for (let j = 0; j < 20; j++) {
+      matrix[i][j] = {
+        bgcolor: "grey",
+        content: `${i},${j}`,
+        name: "name",
+        x: i,
+        y: j,
+      };
+    }
+  }
+</script>
+
+<main>
+  <!-- Renderiza matriz 20 x 20, interactuable ( cuando aprietas encima de una celda, esta cambia de color ) -->
+  {#each matrix as row}
+    <div class="d-flex flex-row justify-center">
+      {#each row as cell}
+        <div
+          class="cell"
+          style="background-color: {cell.bgcolor}"
+          on:click={() => (cell.bgcolor = "white")}
+          on:keypress={() => (cell.bgcolor = "white")}
+        >
+          {cell.content}
+        </div>
+      {/each}
+    </div>
+  {/each}
+</main>
+
+<style>
+  .cell {
+    border: 1px solid black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 10px;
+    margin: 5px;
+    min-width: 30px;
+    min-height: 30px;
+  }
+</style>
---
+++ ejemplo_frontend/front mapa/src/main.js
@@ -0,0 +1,9 @@
+import './app.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App.svelte';
+
+const app = new App({
+  target: document.getElementById('app')
+})
+
+export default app
